7f7d42a3fd5d8c09d65a48c6232c65b9a9249cbb
Daniel Balloch <danielpbballoch@gmail.com>
2024-06-10 16:12:58 +1200

    daily deploy function working

--- functions/daily-deploy/daily-deploy.js
+++ functions/daily-deploy/daily-deploy.js
@@ -1,10 +1,18 @@
 const { schedule } = require('@netlify/functions')
+const fetch = require('node-fetch')
+
+
+const BUILD_HOOK = "https://api.netlify.com/build_hooks/664d391e0ea5405ea0152137"
 
 // To learn about scheduled functions and supported cron extensions,
 // see: https://ntl.fyi/sched-func
 module.exports.handler = schedule('0 0 * * *', async (event) => {
-  const eventBody = JSON.parse(event.body)
-  console.log(`Next function run at ${eventBody.next_run}.`)
+  await fetch(BUILD_HOOK, {
+    method: 'POST'
+  }).then(response => {
+    console.log("Build hook response:", response)
+  })
+
 
   return {
     statusCode: 200,
